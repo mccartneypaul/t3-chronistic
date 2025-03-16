504b0c000d096863ecb4188338c60890716aebea
Paul McCartney <paulmc@1791.com>
2025-03-16 18:37:31 -0400

    feat: pull uploaded s3 image

---
+++ src/app/api/s3/route.ts
@@ -0,0 +1,44 @@
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { NextResponse, type NextRequest } from "next/server";
+
+const client = new S3Client({
+  region: process.env.NEXT_PUBLIC_AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ?? "",
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ?? "",
+  },
+});
+
+export const GET = async (request: NextRequest) => {
+  const { searchParams } = request.nextUrl;
+
+  const file = searchParams.get("file");
+
+  // console.log("file", file);
+  if (!file) {
+    return new Response(
+      JSON.stringify({ error: "File query parameter is required" }),
+      { status: 400, headers: { "Content-Type": "application/json" } }
+    );
+  }
+
+  const command = new GetObjectCommand({
+    Bucket: process.env.NEXT_PUBLIC_AWS_BUCKET_NAME,
+    Key: file,
+  });
+
+  try {
+    const data = await client.send(command);
+    if (!data.Body) {
+      return new NextResponse("Image not found", { status: 404 });
+    }
+
+    const body = await data.Body.transformToByteArray();
+    const headers = new Headers();
+    headers.set("Content-Type", data.ContentType || "image/jpeg");
+    return new NextResponse(body, { status: 200, headers });
+  } catch (error) {
+    console.error("Error fetching image from S3:", error);
+    return new NextResponse("Error fetching image from S3", { status: 500 });
+  }
+};
--- src/components/overview-map.tsx
+++ src/components/overview-map.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
 import ConstructIcon from "./construct-icon";
 import ConstructOverview from "./construct-overview";
-import React, { useEffect } from "react";
+import React, { Suspense, useEffect } from "react";
 import { useConstructContext } from "@chronistic/providers/construct-store-provider";
 import { ActionPallette } from "./action-palette";
 import { translatePositionForView} from "@chronistic/models/Position";
 import { initBoundingBox } from "@chronistic/models/BoundingBox";
 import Draggable, { type DraggableData, type DraggableEvent } from "react-draggable";
 
-const mapImage = "/middleearth.jpg";
+const mapImage = "/api/s3?file=middleearth.jpg";
 
-function OverviewMap() {
+export default function OverviewMap() {
   const nodeRef = React.useRef(null); // To suppress the warning about the ref in strict mode
+  const s3ImageUrl = React.useRef("");
   const isDraggingRef = React.useRef(false);
   const [isOpen, setOpen] = React.useState(false);
   const [viewTransformation, setViewTransformation] = React.useState({scale: 100, translateX: 0, translateY: 0});
@@ -22,7 +23,6 @@ function OverviewMap() {
     ...construct,
     ...translatePositionForView(boundingBox, viewTransformation, construct)
   })));
-
 
   useEffect(() => {
     console.log("OverviewMap re-rendered with constructs:", storeConstructs);
@@ -47,57 +47,57 @@ function OverviewMap() {
 
   return (
     <>
-      <div className="overflow-hidden">
-        <div className={`overflow-hidden aspect-auto relative h-[37vw] transform scale-${viewTransformation.scale}`}>
-          <Draggable
-            nodeRef={nodeRef} // To suppress the warning about the ref in strict mode
-            onStop={onStop}
-            onDrag={onDrag}
-            scale={viewTransformation.scale / 100}
-          >
-            <Image
-              className={`object-contain`}
-              src={mapImage}
-              alt="map"
-              quality="100"
-              ref={nodeRef}
-              fill
-              draggable={false} // Disable default drag behavior
-              onLoad={(e) =>{
-                console.log("Image loaded", e.currentTarget.getBoundingClientRect());
-                setBoundingBox(e.currentTarget.getBoundingClientRect())
-              }
-              }
-            />
-          </Draggable>
+      <Suspense fallback={<p>Loading...</p>}>
+        <div className="overflow-hidden">
+          <div className={`overflow-hidden aspect-auto relative h-[37vw] transform scale-${viewTransformation.scale}`}>
+            <Draggable
+              nodeRef={nodeRef} // To suppress the warning about the ref in strict mode
+              onStop={onStop}
+              onDrag={onDrag}
+              scale={viewTransformation.scale / 100}
+            >
+              <Image
+                className={`object-contain`}
+                src={mapImage}
+                priority
+                alt="map"
+                quality="100"
+                ref={nodeRef}
+                fill
+                draggable={false} // Disable default drag behavior
+                onLoad={(e) =>{
+                  console.log("Image loaded", e.currentTarget.getBoundingClientRect());
+                  setBoundingBox(e.currentTarget.getBoundingClientRect())
+                }
+                }
+              />
+            </Draggable>
+          </div>
         </div>
-      </div>
 
-      {activeConstruct && (
-        <ConstructOverview isOpen={isOpen} setOpen={setOpen} />
-      )}
-      
-      <ActionPallette viewTransformation={viewTransformation} setViewTransformation={setViewTransformation}/>
-      
-      {boundingBox !== initBoundingBox &&
-        translatedConstructs
-          .filter((construct) => construct !== undefined &&
-                                construct.posX >= boundingBox.left &&
-                                construct.posX <= boundingBox.right &&
-                                construct.posY >= boundingBox.top &&
-                                construct.posY <= boundingBox.bottom)
-          .map((construct) => (
-          <ConstructIcon
-            key={construct.id}
-            setOpen={setOpen}
-            constructId={construct.id}
-            boundingBox={boundingBox}
-            viewTransformation={viewTransformation}
-          />
-      ))}
+        {activeConstruct && (
+          <ConstructOverview isOpen={isOpen} setOpen={setOpen} />
+        )}
+        
+        <ActionPallette viewTransformation={viewTransformation} setViewTransformation={setViewTransformation}/>
+        
+        {boundingBox !== initBoundingBox &&
+          translatedConstructs
+            .filter((construct) => construct !== undefined &&
+                                  construct.posX >= boundingBox.left &&
+                                  construct.posX <= boundingBox.right &&
+                                  construct.posY >= boundingBox.top &&
+                                  construct.posY <= boundingBox.bottom)
+            .map((construct) => (
+            <ConstructIcon
+              key={construct.id}
+              setOpen={setOpen}
+              constructId={construct.id}
+              boundingBox={boundingBox}
+              viewTransformation={viewTransformation}
+            />
+        ))}
+      </Suspense>
     </>
-    
   );
 }
-
-export default OverviewMap;
