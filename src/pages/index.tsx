24f6895d3adfb8ad0606700926763917f4265620
Paul McCartney <paulmc@1791.com>
2025-06-24 20:52:30 -0400

    feat: only allow going in if you have a user

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,6 +22,7 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Chronistic
           </h1>
+          <p className="text-3xl text-white">A story tracking tool</p>
         </div>
         <div className="flex flex-col items-center gap-2">
           <AuthShowcase />
@@ -43,15 +44,19 @@ const AuthShowcase: React.FC = () => {
 
   return (
     <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-      </p>
-      <p className="text-2xl text-white">
-        Get started creating your world with Chronistic!
-      </p>
-      <Button variant="contained">
-        <Link href="/world">Get started!</Link>
-      </Button>
+      {sessionData && (
+        <>
+          <span className="text-center text-2xl text-white">
+            Logged in as {sessionData.user?.name}
+          </span>
+          <p className="text-2xl text-white">
+            Get started creating your world with Chronistic!
+          </p>
+          <Button variant="contained">
+            <Link href="/world">Get started!</Link>
+          </Button>
+        </>
+      )}
       <button
         className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
         onClick={sessionData ? () => void signOut() : () => void signIn()}
